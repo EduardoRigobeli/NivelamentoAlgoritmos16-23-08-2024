484b56823ca364718b89caffb26b17ef50eed66f
EduardoRigobeli <eduardo.souza@grupointegrado.br>
2024-08-23 19:53:50 -0300

    Enviando um novo HelloWorld

--- index.java
+++ index.java
@@ -1,6 +1,16 @@
-public class index{
+import java.util.Scanner;
+
+public class index {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Nao");
+        Scanner prompt = new Scanner(System.in);
+        System.out.print("Informe as horas: ");
+        int horas = prompt.nextInt();
+        System.out.print("Informe os minutos: ");
+        int minutos = prompt.nextInt();
+        System.out.print("Informe os segundos: ");
+        int segundos = prompt.nextInt();
+
+        int resultado= (horas*60*60) + (minutos*60) + segundos;
+        System.out.println("Segundos: "+resultado);
     }
 }
